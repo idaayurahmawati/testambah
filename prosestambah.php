67feefe84aec95a1fe7ea2638de93fc2e4f216b3
idaayurahmawati <109191287+idaayurahmawati@users.noreply.github.com>
2022-11-02 07:44:08 +0700

    Add files via upload

---
+++ prosestambah.php
@@ -0,0 +1,9 @@
+<?php
+include("koneksi.php")
+if((isset[$_POST['tambah']]));
+$nama['nama'];
+$jenisbantuan['jenisbantuan'];
+
+$sql="INSET INTO tb_bantuan(nama, jenisbantuan) value ('$nama','$jenibantuan';
+$query="mysqli_query($koneksi,$sql);
+                                                                                                                                                                                                                                                                                                                                                                                                                  
---
+++ tambah.php
@@ -0,0 +1,31 @@
+<DOCTYPE html
+<html lang="en">
+    <head>
+        <title>FORM BANTUAN BLT<title>
+</head>
+<body>
+    <h1>FORM BANTUAN BLT<h1>
+        <form action="prosestambah.php"method="POST">
+        <from>
+            <fieldet>
+             <p>
+                <label  for="nama">Nama Lengkap:</label>
+                <input type="text" name="name"/>
+</p>
+<p>
+    <label for="jenisbantuan">jenisbantuan</label>                              
+    <label type="radio" name="jenisbantuan" value="tunai"/>tunai
+    <label type="radio" name="jenisbantuan" value="nontunai"/>nontunai
+    <label type="radio" name="jenisbantuan" value="beras"/>beras
+    <label type="radio" name="jenisbantuan" value="minyak"/>minyak
+    <label type="radio" name="jeniasbantuan" value="gula"/>gula
+    <label for=""></label>
+</p>
+<p>
+<input type="submit" value="Kirim Bantuan" value name="Kirim Bantuan" />
+</p>
+</form>
+</fieldset>
+</body>
+    </html>
+
